15da3fc692718765c30495fcbaafe8f3123bdf1e
china_wangyu <china_wangyu@aliyun.com>
2017-10-14 13:19:57 +0800

    core框架初始化应用模块 2017-10-14

---
+++ app/conf.php
@@ -0,0 +1,84 @@
+`<?php
+/**
+ * Created by PhpStorm.
+ * User: zhns_
+ * Date: 2017/10/13
+ * Time: 11:37
+ */
+return [
+
+    // 应用调试模式
+    'app_debug'              => true,
+
+    // 控制器类后缀
+    'controller_suffix'      => false,
+
+    // 模型库后缀
+    'model_suffix'           => false,
+
+    // 操作方法后缀
+    'action_suffix'          => '',
+
+    // 应用类库后缀
+    'class_suffix'           => false,
+
+
+    // +----------------------------------------------------------------------
+    // | 模块设置
+    // +----------------------------------------------------------------------
+
+    // 默认模块名
+    'default_module'         => 'index',
+    // 禁止访问模块
+    'deny_module_list'       => ['common'],
+    // 默认控制器名
+    'default_controller'     => 'Index',
+    // 默认操作名
+    'default_action'         => 'index',
+    // 默认验证器
+    'default_validate'       => '',
+    // 默认的空控制器名
+    'empty_controller'       => 'Error',
+
+    // 自动搜索控制器
+    'controller_auto_search' => false,
+
+    'template'               => [
+        // 模板引擎类型 支持 php think 支持扩展
+        'type'         => 'Think',
+        // 模板路径
+        'view_path'    => '',
+        // 模板后缀
+        'view_suffix'  => 'html',
+        // 模板文件名分隔符
+        'view_depr'    => DS,
+        // 模板引擎普通标签开始标记
+        'tpl_begin'    => '{',
+        // 模板引擎普通标签结束标记
+        'tpl_end'      => '}',
+        // 标签库标签开始标记
+        'taglib_begin' => '{',
+        // 标签库标签结束标记
+        'taglib_end'   => '}',
+    ],
+
+    'cache'                  => [
+        // 驱动方式
+        'type'   => 'File',
+        // 缓存保存目录
+        'path'   => CACHE_PATH,
+        // 缓存前缀
+        'prefix' => '',
+        // 缓存有效期 0表示永久缓存
+        'expire' => 0,
+    ],
+
+    'log'                    => [
+        // 日志记录方式，内置 file socket 支持扩展
+        'type'  => 'File',
+        // 日志保存目录
+        'path'  => LOG_PATH,
+        // 日志记录级别
+        'level' => [],
+    ],
+];
