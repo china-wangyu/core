b725f6e854a2a40e82b84c9b5400544a89f21a5f
china_wangyu <china_wangyu@aliyun.com>
2017-10-14 13:20:53 +0800

    core框架入口 2017-10-14

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhns_
+ * Date: 2017/9/26
+ * Time: 14:25
+ */
+# 定义项目目录
+define('APP',    'app');
+define('DEBUG', True);
+
+
+require_once './core/start.php';
